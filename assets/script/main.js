74f6a6fa176aac576b162d7e523e93376a723724
Ihor Lev <igorlevin000@gmail.com>
2023-09-07 00:19:28 +0300

    add email-form & white theme

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,10 @@
+// mobile menu script
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('open');
+})
+
 // top scroll
 const btnUp = {
     el: document.querySelector('.btn-up'),
@@ -23,9 +30,26 @@ const btnUp = {
 }
 btnUp.addEventListener();
 
-// mobile menu script
-const burger = document.querySelector('.burger');
-burger.addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('open');
-})
+// theme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+      document.getElementById('slider').checked = true;
+    }
+})();
