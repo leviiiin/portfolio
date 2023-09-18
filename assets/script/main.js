976ea73d292b8ee3e2dd10ae43ad4afbcf7b1f7a
Ihor Lev <igorlevin000@gmail.com>
2023-09-18 22:44:52 +0300

    global update

--- assets/script/main.js
+++ assets/script/main.js
@@ -30,31 +30,6 @@ burger.addEventListener('click', function () {
     })
 })
 
-// top scroll
-const btnUp = {
-    el: document.querySelector('.btn-up'),
-    show() {
-        this.el.classList.remove('btn-up_hide');
-    },
-    hide() {
-        this.el.classList.add('btn-up_hide');
-    },
-    addEventListener() {
-        window.addEventListener('scroll', () => {
-            const scrollY = window.scrollY || document.documentElement.scrollTop;
-            scrollY > 500 ? this.show() : this.hide();
-        });
-        document.querySelector('.btn-up').onclick = () => {
-            window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: 'smooth'
-            });
-        }
-    }
-}
-btnUp.addEventListener();
-
 // theme
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
@@ -78,3 +53,58 @@ function toggleTheme() {
         document.getElementById('slider').checked = true;
     }
 })();
+
+
+// text animation
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+let options = {
+    threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.text-animation');
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+
+function onEntryTwo(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('block-show');
+        }
+    });
+}
+let options2 = {
+    threshold: [0.5]
+};
+let observer2 = new IntersectionObserver(onEntryTwo, options);
+let elements2 = document.querySelectorAll('.block-animation');
+for (let elm of elements2) {
+    observer2.observe(elm);
+}
+
+
+SmoothScroll({
+    animationTime: 400,
+    stepSize: 100,
+    accelerationDelta: 30,
+    accelerationMax: 2,
+    touchpadSupport: true,
+})
+
+function scrollNav() {
+    $('#m-li a').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 160
+        }, 300);
+        return false;
+    });
+}
+scrollNav();
+
